cb57ca705fb5960b1ff18e3f576b7f98d69c8985
Axell Padilla <68310020+axellpadilla@users.noreply.github.com>
2026-03-29 15:58:46 -0600

    Remove Docker commands from setup_env.sh
    Removed Docker compose build and up commands from setup script.

--- .devcontainer/setup_env.sh
+++ .devcontainer/setup_env.sh
@@ -8,9 +8,6 @@ fi
 sudo apt-get update
 sudo apt-get install -y libltdl7 libkrb5-3 libgssapi-krb5-2
 
-docker compose build
-docker compose up -d
-
 export UV_VENV_CLEAR=1
 uv venv .venv
 uv pip install --python .venv/bin/python -r dev_requirements.txt
